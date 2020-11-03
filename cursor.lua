9a2f974a1cb62bb21fb3dc108f4d28397957fd06
Benjamin F. Stigsen <benjamin.stigsen@gmail.com>
2020-11-03 19:30:54 +0100

    cursor.lua changes - chaining functions is now supported (each function returns a cursor) - cursor.set() now resets the position and applies the new position - cursor.print() resets x position back to start of where string was printed - cursor.print_at() now works properly - string.count() was added to count substrings in strings

--- cursor.lua
+++ cursor.lua
@@ -3,6 +3,8 @@
 
     to-do:
         simplify cursor.move()
+        simplify cursor.set()
+        fix increment being added when x or y is 0
 
     usage:
         require("cursor")
@@ -22,9 +24,10 @@ os.execute("")
 cursor = {x = 0, y = 0}
 
 cursor.set = function (cursor, _x, _y)
-    cursor = {x = _x, y = _y}
-    io.write(("\27[%dC\27[%dB"):format(_x, _y))
+    io.write(("\27[%dD\27[%dA\27[%dC\27[%dB"):format(cursor.x, cursor.y, _x, _y))
     io.flush()
+    cursor.x, cursor.y = _x, _y
+    return cursor
 end
 
 cursor.move = function (cursor, steps_x, steps_y)
@@ -55,35 +58,45 @@ cursor.move = function (cursor, steps_x, steps_y)
     end
 
     io.flush()
+    return cursor
 end
 
 cursor.reset = function (cursor)
-    io.write("\27[1000D\27[1000A")
+    io.write(("\27[%dD\27[%dA"):format(cursor.x, cursor.y))
     io.flush()
-    cursor = {x = 0, y = 0}
+    cursor.x, cursor.y = 0, 0
+    return cursor
 end
 
 cursor.reset_x = function (cursor)
     io.write("\27[1000D")
     io.flush()
     cursor.x = 0
+    return cursor
 end
 
 cursor.reset_y = function (cursor)
     io.write("\27[1000A")
     io.flush()
     cursor.y = 0
+    return cursor
 end
 
 cursor.print = function (cursor, msg)
-    io.write(msg)
+    local length = msg:len()
+    io.write(("%s\27[%dD"):format(msg, length))
     io.flush()
+    return cursor
 end
 
 cursor.print_at = function (cursor, msg, x, y)
-    local old = cursor
-    cursor:reset()
-    io.write(("\27[%dC\27[%dB%s"):format(x, y, msg))
+    local length = msg:len()
+    io.write(("\27[%dD\27[%dA\27[%dC\27[%dB%s"):format(cursor.x, cursor.y, x, y, msg))
+    io.write(("\27[%dD\27[%dA\27[%dC\27[%dB"):format(x + length, y, cursor.x, cursor.y))
     io.flush()
-    cursor = old
+    return cursor
+end
+
+string.count = function (str, pattern)
+    return select(2, str:gsub(pattern, ""))
 end
