138c80b5234006922ca29bcec1b51f24691a5598
Benjamin "Tearzz" Stigsen <benjamin.stigsen@gmail.com>
2020-11-12 14:36:49 +0100

    cursor.lua simplified significantly - cursor.reset now simply sets the position to top-left corner - cursor.print_at now sets position, prints, sets position back to previous position

--- cursor.lua
+++ cursor.lua
@@ -41,56 +41,36 @@ cursor.move = function (cursor, _x, _y)
     end
 
     io.flush()
+    
     cursor.x, cursor.y = cursor.x + _x, cursor.y + _y
+    
     return cursor
 end
 
 cursor.reset = function (cursor)
-    if cursor.x ~= 0 then io.write(("\27[%dD"):format(cursor.x)) end
-    if cursor.y ~= 0 then io.write(("\27[%dA"):format(cursor.y)) end
-
+    io.write("\27[1;1H")
     io.flush()
+    
     cursor.x, cursor.y = 0, 0
+    
     return cursor
 end
 
 cursor.print = function (cursor, msg)
     local length = msg:len()
+    
     io.write(("%s\27[%dD"):format(msg, length))
     io.flush()
+    
     return cursor
 end
 
 cursor.print_at = function (cursor, _x, _y, msg)
     local length = msg:len()
 
-    if _x < cursor.x then
-        io.write(("\27[%dD"):format(cursor.x - _x))
-    elseif _x > cursor.x then 
-        io.write(("\27[%dC"):format(_x - cursor.x))
-    end
-
-    if _y < cursor.y then 
-        io.write(("\27[%dA"):format(cursor.y - _y)) 
-    elseif _y > cursor.y then 
-        io.write(("\27[%dB"):format(_y - cursor.y)) 
-    end
-
+    io.write(("\27[%d;%dH"):format(_y + 1, _x + 1))
     io.write(msg)
-    _x = _x + length
-
-    if _x > cursor.x then
-        io.write(("\27[%dD"):format(_x - cursor.x))
-    elseif _x < cursor.x then
-        io.write(("\27[%dC"):format(cursor.x - _x))
-    end
-
-    if _y > cursor.y then
-        io.write(("\27[%dA"):format(_y - cursor.y)) 
-    elseif _y < cursor.y then 
-        io.write(("\27[%dB"):format(cursor.y - _y)) 
-    end
-
+    io.write(("\27[%d;%dH"):format(cursor.x + 1, cursor.y + 1))
     io.flush()
 
     return cursor
